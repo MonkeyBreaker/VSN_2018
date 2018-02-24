86ab3ab9c629c8c7a00396d06fc752e09f57a19d
MonkeyBreaker93 <julian.burellaperez@master.hes-so.ch>
2018-02-24 12:53:10 +0100

    [Comparator] Add generic implementation of the comparator
    Signed-off-by: MonkeyBreaker93 <julian.burellaperez@master.hes-so.ch>

--- src_tb/common_lib/comparator_pkg.vhd
+++ src_tb/common_lib/comparator_pkg.vhd
@@ -18,13 +18,19 @@ package body comparator_pkg is
 
     procedure compare(logger: inout logger_pkg.logger_t; value1 : data_type; value2: data_type) is
     begin
-        -- TODO : Complete
+        if(true = compare(value1, value2)) then
+            logger.log_note(LF & "val1 : " & LF & data_to_string(value1) & LF & "val2 : " & LF &
+            data_to_string(value2) & LF & "are equal" & LF);
+        else
+            logger.log_error(LF & "val1 : " & LF & data_to_string(value1) & LF & "val2 : " & LF &
+            data_to_string(value2) & LF & "are not equal" & LF);
+        end if;
     end compare;
 
 
     function compare(value1 : data_type; value2: data_type) return boolean is
     begin
-        -- TODO : Complete
+        return value1 = value2;
     end compare;
 
 end comparator_pkg;
