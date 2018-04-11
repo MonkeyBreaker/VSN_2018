4ded4a569387a224703bdf1b9cc7b2e590202ec5
MonkeyBreaker93 <julian.burellaperez@master.hes-so.ch>
2018-04-11 18:56:10 +0200

    [SPIKE] Add visual comparator on the scoreboard
    At the moment when I received the data from monitor 1, I will have the corresponding chunk from monitor 0
    
    Signed-off-by: MonkeyBreaker93 <julian.burellaperez@master.hes-so.ch>

--- labo3/src_tb/scoreboard_pkg.vhd
+++ labo3/src_tb/scoreboard_pkg.vhd
@@ -30,10 +30,13 @@ package body scoreboard_pkg is
     procedure scoreboard(variable fifo_input  : inout work.input_transaction_fifo_pkg.tlm_fifo_type;
                          variable fifo_output : inout work.output_transaction_fifo_pkg.tlm_fifo_type
     ) is
+        -- maybe possible only array
+        variable fifo_monitor_0 : work.input_transaction_fifo_pkg.tlm_fifo_type;
         variable trans_input  : input_transaction_t;
         variable trans_output : output_transaction_t;
-        variable counter_out      : integer;
-        variable counter_in      : integer;
+        variable counter_out         : integer;
+        variable counter_in          : integer;
+        variable counter_fifo_0      : integer;
         variable expected     : std_logic_vector(7 downto 0);
     begin
 
@@ -41,17 +44,43 @@ package body scoreboard_pkg is
 
         counter_in := 0;
         counter_out := 0;
+        counter_fifo_0 := 0;
 
         while true loop
 
-            blocking_get(fifo_input, trans_input);
-            logger.log_note("[Scoreboard] received monitor 0 " & integer'image(counter_out));
-            counter_out := counter_out + 1;
-
             if(fifo_output.is_empty = false) then
               blocking_get(fifo_output, trans_output);
-              logger.log_note("[Scoreboard] received monitor 1 " & integer'image(counter_in));
-              counter_in := counter_in + 1;
+              logger.log_note("[Scoreboard] received monitor 1 " & integer'image(counter_out));
+
+              -- while(counter_fifo_0 > 150) loop
+              --  blocking_get(fifo_monitor_0, trans_input);
+              --   counter_fifo_0 := counter_fifo_0-1;
+              -- end loop;
+
+              for i in 0 to (trans_output.data_out_trans'length)-1 loop
+                blocking_get(fifo_monitor_0, trans_input);
+                logger.log_note("[Scoreboard] monitor 0, value received : " & integer'image(to_integer(signed(trans_input.data_in_trans))));
+                logger.log_note("[Scoreboard] monitor 1, value received : " & integer'image(to_integer(signed(trans_output.data_out_trans(i)))));
+                counter_fifo_0 := counter_fifo_0-1;
+              end loop;
+
+              counter_out := counter_out + 1;
+            end if;
+
+            blocking_get(fifo_input, trans_input);
+            logger.log_note("[Scoreboard] received monitor 0 " & integer'image(counter_in));
+            counter_in := counter_in + 1;
+
+            logger.log_note("[Scoreboard] monitor 0, value received : " & integer'image(to_integer(signed(trans_input.data_in_trans))));
+
+            if(counter_fifo_0 < 200) then
+              blocking_put(fifo_monitor_0, trans_input);
+              counter_fifo_0 := counter_fifo_0+1;
+            else
+              -- Pseudo Circular buffer
+              blocking_put(fifo_monitor_0, trans_input);
+
+              blocking_get(fifo_monitor_0, trans_input);
             end if;
 
         end loop;
